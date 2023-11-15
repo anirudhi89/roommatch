3f1bc7abdedb59bb9c6a35101575a9b4f51f74bb
ctf16 <ctf16@pitt.edu>
2023-11-14 21:40:45 -0500

    NavBar and NavBarExpanded widgets now usable

--- lib/Widgets/navBar.dart
+++ lib/Widgets/navBar.dart
@@ -2,79 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:roommatch/Views/myChats.dart';
 import 'package:roommatch/Views/myProfile.dart';
 import 'package:roommatch/Views/swipeOnPpl.dart';
+import 'package:roommatch/Views/userPreferences.dart';
+import 'package:roommatch/Views/appSettings.dart';
 
-void main() => runApp(const NavigationBarApp());
-
-class NavigationBarApp extends StatelessWidget {
-  const NavigationBarApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(home: NavigationExample());
-  }
-}
-
-class NavigationExample extends StatefulWidget {
-  const NavigationExample({super.key});
-
-  @override
-  State<NavigationExample> createState() => _NavigationExampleState();
-}
-
-class _NavigationExampleState extends State<NavigationExample> {
-  int currentPageIndex = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      bottomNavigationBar: NavigationBar(
-        onDestinationSelected: (int index) {
-          setState(() {
-            //Profile screen
-            if (currentPageIndex != 0 && index == 0) {
-              currentPageIndex = index;
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => MyProfile()),
-              );
-            } else if (currentPageIndex != 1 && index == 1) {
-              currentPageIndex = index;
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => SwipeOnPpl()),
-              );
-            } else if (currentPageIndex != 2 && index == 2) {
-              currentPageIndex = index;
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => MyChats()),
-              );
-            }
-          });
-        },
-        indicatorColor: Colors.amber[800],
-        selectedIndex: currentPageIndex,
-        destinations: const <Widget>[
-          NavigationDestination(
-            icon: Icon(Icons.person_sharp),
-            label: 'Profile',
-          ),
-          NavigationDestination(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          NavigationDestination(
-            icon: Icon(Icons.message_outlined),
-            label: 'Chats',
-          ),
-        ],
-      ),
-      //Upon pressing profile, it will take you to the profile page (myProfile.dart)
-      //Upon pressing home, it will take you to the home swipe page (swipeOnPpl.dart)
-      //Upon pressing chats, it will take you to the chats page (myChats.dart)
-    );
-  }
-}
+/*
+Generic NavBar widget that will fit as the bottomNavigationBar for several of our screens
+Has 3 sections: Profile, Home, and Chats
+Takes in one parameter (tab) so each screen is initialized with the correct option highlighted
+*/
 
 class NavBar extends StatefulWidget {
   const NavBar({super.key, required int tab});
@@ -84,7 +19,7 @@ class NavBar extends StatefulWidget {
 }
 
 class _NavBarState extends State<NavBar> {
-  int currentPageIndex = 0;
+  int currentPageIndex = 1;
 
   @override
   Widget build(BuildContext context) {
@@ -111,8 +46,8 @@ class _NavBarState extends State<NavBar> {
           }
         });
       },
-      indicatorColor: Colors.amber[800],
-      selectedIndex: currentPageIndex,
+      indicatorColor: Color.fromARGB(255, 34, 188, 222),
+      selectedIndex: widget.tab,
       destinations: const <Widget>[
         NavigationDestination(
           icon: Icon(Icons.person_sharp),
@@ -121,6 +56,86 @@ class _NavBarState extends State<NavBar> {
         NavigationDestination(
           icon: Icon(Icons.home),
           label: 'Home',
+        ),
+        NavigationDestination(
+          icon: Icon(Icons.message_outlined),
+          label: 'Chats',
+        ),
+      ],
+    );
+  }
+}
+
+/*
+Slightly altered NavBar for the Profile page, with options to open Preferences as well as Settings
+Has 5 sections: Profile, Preferences, Home, Settings, Chats
+Similarly takes in one parameter
+*/
+class NavBarExpanded extends StatefulWidget {
+  final int tab;
+
+  const NavBarExpanded({Key? key, required this.tab}) : super(key: key);
+
+  @override
+  State<NavBarExpanded> createState() => _NavBarExpandedState();
+}
+
+class _NavBarExpandedState extends State<NavBarExpanded> {
+  int currentPageIndex = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return NavigationBar(
+      onDestinationSelected: (int index) {
+        setState(() {
+          currentPageIndex = index;
+          //Profile screen
+          if (currentPageIndex == 0) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => MyProfile()),
+            );
+          } else if (currentPageIndex == 1) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => UserPreferences()),
+            );
+          } else if (currentPageIndex == 2) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => SwipeOnPpl()),
+            );
+          } else if (currentPageIndex == 3) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => AppSettings()),
+            );
+          } else if (currentPageIndex == 4) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => MyChats()),
+            );
+          }
+        });
+      },
+      indicatorColor: Color.fromARGB(255, 34, 188, 222),
+      selectedIndex: widget.tab,
+      destinations: const <Widget>[
+        NavigationDestination(
+          icon: Icon(Icons.person_sharp),
+          label: 'Profile',
+        ),
+        NavigationDestination(
+          icon: Icon(Icons.display_settings_sharp),
+          label: 'Preferences',
+        ),
+        NavigationDestination(
+          icon: Icon(Icons.home),
+          label: 'Home',
+        ),
+        NavigationDestination(
+          icon: Icon(Icons.settings),
+          label: 'Settings',
         ),
         NavigationDestination(
           icon: Icon(Icons.message_outlined),
