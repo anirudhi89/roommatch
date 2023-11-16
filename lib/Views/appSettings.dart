68a168a20c84c7361800b9d3867dfdde1e7600b8
ctf16 <ctf16@pitt.edu>
2023-11-15 20:25:31 -0500

    Added switches to arbitrary settings

--- lib/Views/appSettings.dart
+++ lib/Views/appSettings.dart
@@ -11,40 +11,63 @@ class AppSettings extends StatelessWidget {
       appBar: AppBar(title: const Text('App Settings')),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.end,
           children: <Widget>[
-            const Text(
-              'App Settings',
-              style: TextStyle(fontSize: 40),
-            ),
             const SizedBox(height: 30),
             const Text(
               'Setting 1',
               style: TextStyle(fontSize: 20),
             ),
+            const SettingSwitch(title: "Setting 1"),
             const SizedBox(height: 30),
             const Text(
               'Setting 2',
               style: TextStyle(fontSize: 20),
             ),
+            const SettingSwitch(title: "Setting 2"),
             const SizedBox(height: 30),
             const Text(
               'Setting 3',
               style: TextStyle(fontSize: 20),
             ),
+            const SettingSwitch(title: "Setting 3"),
             const SizedBox(height: 30),
             const Text(
               'Setting 4',
               style: TextStyle(fontSize: 20),
             ),
-            const SizedBox(height: 30),
-            const Text(
-              'Setting 5',
-              style: TextStyle(fontSize: 20),
-            ),
+            const SettingSwitch(title: "Setting 4"),
           ],
         ),
       ),
+    );
+  }
+}
+
+//Quick widget for a setting switch
+//Takes in a title
+
+class SettingSwitch extends StatefulWidget {
+  final String title;
+  const SettingSwitch({Key? key, required this.title}) : super(key: key);
+
+  @override
+  _SettingSwitchState createState() => _SettingSwitchState();
+}
+
+class _SettingSwitchState extends State<SettingSwitch> {
+  bool _value = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Switch(
+      value: _value,
+      onChanged: (value) {
+        setState(() {
+          _value = value;
+        });
+      },
     );
   }
 }
