22fa30020b93e7e4744945df43f65430b209e66c
ctf16 <ctf16@pitt.edu>
2023-12-10 18:44:03 -0500

    Basic name and age changers added

--- lib/Views/myProfile.dart
+++ lib/Views/myProfile.dart
@@ -5,6 +5,8 @@ import 'package:roommatch/Models/MainUserModel.dart';
 import 'package:roommatch/ViewModels/MainUserViewModel.dart';
 import 'package:roommatch/Widgets/clickableImage.dart';
 import 'package:roommatch/Widgets/navBar.dart';
+import 'package:roommatch/Widgets/ageDropdown.dart';
+import 'package:roommatch/Widgets/nameChange.dart';
 
 class MyProfile extends StatelessWidget {
   const MyProfile({Key? key}) : super(key: key);
@@ -12,6 +14,10 @@ class MyProfile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     MainUserModel mainUser = MainUserViewModel().getUser();
+    TextEditingController textcontroller_fn =
+        TextEditingController(text: '${mainUser.firstName} ');
+    TextEditingController textcontroller_ln =
+        TextEditingController(text: '${mainUser.lastName}');
     return Scaffold(
       appBar: AppBar(
         title: const Text('My Profile'),
@@ -22,7 +28,7 @@ class MyProfile extends StatelessWidget {
         children: <Widget>[
           SizedBox(
             //SizedBox for 3x3 grid of images
-            height: 405,
+            height: 400,
             width: 400,
             child: GridView.count(
               crossAxisCount: 3,
@@ -45,8 +51,26 @@ class MyProfile extends StatelessWidget {
             //Align to the left
             //Age: dropdown menu?
             //Name: editable text box
-            height: 60,
-            child: Text('${mainUser.firstName} ${mainUser.lastName}, 21'),
+            height: 50,
+            child: Align(
+              alignment: Alignment.topLeft,
+              child: DecoratedBox(
+                decoration: BoxDecoration(
+                  color: Colors.lightBlueAccent,
+                ),
+                child: Row(
+                  children: <Widget>[
+                    NameChange(),
+                    SizedBox(width: 15),
+                    SizedBox(
+                      height: 50,
+                      width: 60,
+                      child: AgeDropdown(currentAge: mainUser.age),
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ),
           SizedBox(
               //SizedBox for institution
@@ -54,6 +78,13 @@ class MyProfile extends StatelessWidget {
               ),
           SizedBox(
               //SizedBox for bio
+              ),
+          SizedBox(
+              //Final box for a "Save changes" button
+              //Pressing the button will save:
+              //1. First and last name changes (from texteditingcontroller.text)
+              //2. Institution name change
+              //3. Changes in bio
               ),
         ],
       ),
