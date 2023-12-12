e2336cc1ccf391d4b1f03745ffc33a0ae3eb47e2
anirudhi89 <anirudh.iyer@outlook.com>
2023-12-12 08:34:12 -0500

    add chats/messages

--- lib/Views/MessageScreen.dart
+++ lib/Views/MessageScreen.dart
@@ -65,6 +65,7 @@ class _MessageScreenState extends State<MessageScreen> {
                 String message = textEditingController.text;
 
                 widget.onMessageSent(message);
+<<<<<<< HEAD
                 textEditingController.clear();
 
                 Future.delayed(Duration(milliseconds: 300), () {
@@ -72,6 +73,10 @@ class _MessageScreenState extends State<MessageScreen> {
                     setState(() {});
                   }
                 });
+=======
+
+                textEditingController.clear();
+>>>>>>> 78978ec (add chats/messages)
               },
             ),
           ],
