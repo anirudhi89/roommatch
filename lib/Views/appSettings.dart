38ef87a73cbbb434cda48d97857789e8411e498a
ctf16 <ctf16@pitt.edu>
2023-12-12 01:25:00 -0500

    Added uname + pwd to settings

--- lib/Views/appSettings.dart
+++ lib/Views/appSettings.dart
@@ -1,43 +1,79 @@
 //Simple skeleton view for the app settings page
 
 import 'package:flutter/material.dart';
+import 'package:roommatch/Models/MainUserModel.dart';
+import 'package:roommatch/ViewModels/MainUserViewModel.dart';
 
 class AppSettings extends StatelessWidget {
-  const AppSettings({Key? key}) : super(key: key);
+  AppSettings({Key? key}) : super(key: key);
+  MainUserModel mainuser = MainUserViewModel().getUser();
 
   @override
   Widget build(BuildContext context) {
+    TextEditingController username_controller =
+        TextEditingController(text: '${mainuser.userName}');
+    TextEditingController password_controller =
+        TextEditingController(text: '${mainuser.password}');
+
     return Scaffold(
-      appBar: AppBar(title: const Text('App Settings')),
+      appBar: AppBar(
+        title: const Text('App Settings'),
+        backgroundColor: Colors.lightBlue[800],
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             const SizedBox(height: 30),
+            TextField(
+              controller: username_controller,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Username',
+              ),
+              style: TextStyle(
+                color: Colors.black,
+                fontFamily: 'Roboto',
+                fontWeight: FontWeight.bold,
+              ),
+              onChanged: (String? newusername) {
+                mainuser.userName = newusername!;
+                debugPrint(
+                    '[uid=${mainuser.userID}] Username changed to $newusername');
+              },
+            ),
+            const SizedBox(height: 20),
+            TextField(
+              obscureText: true,
+              controller: password_controller,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Password',
+              ),
+              style: TextStyle(
+                color: Colors.black,
+                fontFamily: 'Roboto',
+                fontWeight: FontWeight.bold,
+              ),
+              onChanged: (String? newpassword) {
+                mainuser.password = newpassword!;
+                debugPrint(
+                    '[uid=${mainuser.userID}] Password changed to $newpassword');
+              },
+            ),
+            const SizedBox(height: 20),
             const Text(
               'Setting 1',
               style: TextStyle(fontSize: 20),
             ),
             const SettingSwitch(title: "Setting 1"),
-            const SizedBox(height: 30),
+            const SizedBox(height: 20),
             const Text(
               'Setting 2',
               style: TextStyle(fontSize: 20),
             ),
             const SettingSwitch(title: "Setting 2"),
-            const SizedBox(height: 30),
-            const Text(
-              'Setting 3',
-              style: TextStyle(fontSize: 20),
-            ),
-            const SettingSwitch(title: "Setting 3"),
-            const SizedBox(height: 30),
-            const Text(
-              'Setting 4',
-              style: TextStyle(fontSize: 20),
-            ),
-            const SettingSwitch(title: "Setting 4"),
           ],
         ),
       ),
@@ -63,6 +99,7 @@ class _SettingSwitchState extends State<SettingSwitch> {
   Widget build(BuildContext context) {
     return Switch(
       value: _value,
+      activeColor: Colors.lightBlue[800],
       onChanged: (value) {
         setState(() {
           _value = value;
